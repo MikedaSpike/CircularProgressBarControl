e2ed529b5665f56052d963346bf598a9f8f9f13a
Mike <48748234+MikedaSpike@users.noreply.github.com>
2024-09-14 05:46:05 +0200

    Added CancelledAdded Cancelled property to circular progress bar

--- CircularProgressBarControl/CircularProgressBarControl.vb
+++ CircularProgressBarControl/CircularProgressBarControl.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.Drawing
 Imports System.Drawing.Drawing2D
+Imports System.Resources
 Imports System.Windows.Forms
 
 Public Class CircularProgressBarControl
@@ -12,6 +13,7 @@ Public Class CircularProgressBarControl
     Private _fontStyle As Font = New Font("Arial", 12, FontStyle.Bold)
     Private _displayText As String = "0%"
     Private _displayDuration As Integer = 2000 ' Duration in milliseconds
+    Private _cancelled As Boolean = False
     Private hideTimer As System.Windows.Forms.Timer
 
     Public Property Progress As Integer
@@ -83,11 +85,32 @@ Public Class CircularProgressBarControl
         End Set
     End Property
 
+    Public Property Cancelled As Boolean
+        Get
+            Return _cancelled
+        End Get
+        Set(ByVal value As Boolean)
+            _cancelled = value
+            If _cancelled Then
+                _displayText = "Cancelled"
+                StartHideTimer()
+            End If
+            Me.Invalidate()
+        End Set
+    End Property
+
     Protected Overrides Sub OnPaint(ByVal e As PaintEventArgs)
         MyBase.OnPaint(e)
         e.Graphics.SmoothingMode = SmoothingMode.AntiAlias
 
-        ' Draw the progress arc
+        ' Draw the inner circle first, slightly smaller than the progress bar
+        Dim innerCircleMargin As Integer = _lineThickness / 2
+        Using brush As New SolidBrush(_innerCircleColor)
+            Dim innerRect As New Rectangle(innerCircleMargin, innerCircleMargin, Me.Width - 2 * innerCircleMargin, Me.Height - 2 * innerCircleMargin)
+            e.Graphics.FillEllipse(brush, innerRect)
+        End Using
+
+        ' Draw the progress arc on top of the inner circle
         Using path As New GraphicsPath()
             path.AddArc(CSng(_lineThickness / 2), CSng(_lineThickness / 2), CSng(Me.Width - _lineThickness), CSng(Me.Height - _lineThickness), -90, 360 * (_progress / 100.0F))
             Using pen As New Pen(_lineColor, _lineThickness) With {.StartCap = LineCap.Round, .EndCap = LineCap.Round}
@@ -95,17 +118,16 @@ Public Class CircularProgressBarControl
             End Using
         End Using
 
-        ' Draw the inner circle
-        Using brush As New SolidBrush(_innerCircleColor)
-            Dim innerRect As New Rectangle(_lineThickness, _lineThickness, Me.Width - 2 * _lineThickness, Me.Height - 2 * _lineThickness)
-            e.Graphics.FillEllipse(brush, innerRect)
-        End Using
-
-        ' Draw the text
+        ' Draw the text on top of everything
         Using brush As New SolidBrush(Me.ForeColor)
+            Dim fontSize As Single = _fontStyle.Size
             Dim textSize As SizeF = e.Graphics.MeasureString(_displayText, _fontStyle)
+            While textSize.Width > Me.Width - _lineThickness * 2 AndAlso fontSize > 1
+                fontSize -= 1
+                textSize = e.Graphics.MeasureString(_displayText, New Font(_fontStyle.FontFamily, fontSize, _fontStyle.Style))
+            End While
             Dim textPoint As New PointF((Me.Width - textSize.Width) / 2, (Me.Height - textSize.Height) / 2)
-            e.Graphics.DrawString(_displayText, _fontStyle, brush, textPoint)
+            e.Graphics.DrawString(_displayText, New Font(_fontStyle.FontFamily, fontSize, _fontStyle.Style), brush, textPoint)
         End Using
     End Sub
 
